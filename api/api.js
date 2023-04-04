447a4ce2f0faf2a5561000cdc679b6c4552436f4
Lucas-Debiais <lucas.debiais11@gmail.com>
2023-04-04 10:04:00 +0200

    feat: add server

---
+++ api/api.js
@@ -0,0 +1,69 @@
+import express from "express";
+import {PrismaClient} from '@prisma/client'
+import cors from 'cors'
+
+const prisma = new PrismaClient();
+
+// Création de l'application
+const app = express();
+
+// Configuration des middleware
+app.use(express.json());
+const authorizationMiddleware = (req, res, next) => {
+    // if(req.headers.authorization !== password) {
+    //     return res.status(401).send("Unauthorized");
+    // }
+    return next();
+}
+
+app.use(cors({
+    origin: '*'
+}));
+
+// Configuration des routes
+app.get("/health", (req, res) => {
+    res.send("OK");
+});
+
+//Création d'une donnée
+app.post("/api/data", authorizationMiddleware, async (req, res) => {
+    const {name} = req.body;
+    if (typeof name !== "string"){
+        return res.status(400).send({400:"Name should be a string"})
+    }
+    try {
+        const result = await prisma.result.create({data: {name: req.body.name}});
+        res.json(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e);
+    }
+});
+
+//Lister les données
+app.get("/api/data", authorizationMiddleware, async (req, res) => {
+    try {
+        const result = await prisma.result.findMany({take: 100, orderBy: {name: "asc"}});
+        res.json(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e);
+    }
+});
+
+//Suppression d'une donnée
+app.delete("/api/:dataName", authorizationMiddleware, async(req, res) => {
+    try {
+        const {dataName} = req.params;
+        const result = await prisma.result.delete({where: {name: dataName}});
+        res.json(result);
+    } catch (e) {
+        console.error(e);
+        res.status(500).send(e);
+    }
+});
+
+// Lancement du server
+app.listen(3000, () => {
+    console.log('Server started !');
+});
