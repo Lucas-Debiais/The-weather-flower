b3b2e56b3c6b9e9c26e80167a4314184d07fdb39
OuahElPato <115996568+OuahElPato@users.noreply.github.com>
2023-04-06 11:22:39 +0200

    Integration de notre maquette

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import {useState} from 'react'
+import {getCurrentDate} from './utils/CurrentDate'
 import './App.scss'
 
 function App() {
+    return (
+        <div className="App">
+            <div className={'title'}>
+                <h3>9:41 am</h3>
+                <h4>Sunset Time, Wednesday</h4>
+            </div>
+            <div className={'weather_block_bg'}>
+                <div className={'weather_block'}>
+                    <div className={'cloud'}></div>
+                    <h2>10°</h2>
+                    <h3>Nantes, France</h3>
+                </div>
+            </div>
+            <div className={'weather_block_information'}>
+                <div className={''}>
+                    <h5>Température</h5>
+                </div>
 
-  return (
-    <div className="App">
-    </div>
-  )
+            </div>
+            {/*{getCurrentDate()}*/}
+        </div>
+    )
 }
 
 export default App
